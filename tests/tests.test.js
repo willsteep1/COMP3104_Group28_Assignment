c75b632b33f767bcdd93f8ab2c1dfe0b67ea44cd
WillingtonSt <145478368+WillingtonSt@users.noreply.github.com>
2024-10-07 15:41:38 -0400

    fixing error with test script

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -3,7 +3,14 @@ const path = require('path');
 
 test('all files within this directory should follow the studentid_gb.txt format', () => {
     const files = fs.readdirSync('./content');
-    files.array.forEach(file => {
+
+    if(files.length === 0) {
+        console.warn('The content directory is empty.');
+        return
+    }
+
+
+    files.forEach(file => {
         const regex = /^\d+_(gb|devops|sdlc)\.txt$/;
         expect(regex.test(file)).toBe(true);
         
